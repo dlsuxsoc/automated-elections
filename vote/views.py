05747ac01543f9a7418e441a390d40cd5583cdca
Alexczar Dela Torre <alexczar_delatorre@dlsu.edu.ph>
2018-05-19 01:33:41 +0800

    Set the email backend up.

--- vote/views.py
+++ vote/views.py
@@ -1,7 +1,23 @@
 # Create your views here.
+from django.conf import settings
+
+# Send an email receipt containing the candidates the voter voted for
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import redirect, render
+from django.core.mail import send_mail
+from django.shortcuts import render, redirect
 from django.views import View
+
+
+def send_email_receipt(user, voted):
+    from_email = settings.EMAIL_HOST_USER
+    to_email = [user.username + '@dlsu.edu.ph']
+    subject = '[COMELEC] Voter\'s receipt'
+    message = '''Good day, {0} {1}!,\n\nThank you for voting! You have voted for the following candidates:\n\n{2}''' \
+        .format(
+        user.first_name, user.last_name,
+        voted)
+
+    send_mail(subject=subject, from_email=from_email, recipient_list=to_email, message=message, fail_silently=False)
 
 
 # TODO: If the user who just logged in is not a voter, redirect to the dashboard/administration page
@@ -21,6 +37,7 @@ class VoteView(LoginRequiredMixin, View):
         # Save voter submission
 
         # Send email receipt
+        send_email_receipt(request.user, request.POST)
 
         # Log the user out
         return redirect('logout:logout_voter')
